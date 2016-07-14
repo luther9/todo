e810f645c52930457c3aea524a4096f64aa98596
Luther <lutheroto@gmail.com>
2016-07-14 02:22:10 -0400

    Migrate complete field to item.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712080937) do
+ActiveRecord::Schema.define(version: 20160714055822) do
 
   create_table "items", force: :cascade do |t|
     t.string   "body"
     t.integer  "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "complete"
   end
 
   add_index "items", ["list_id"], name: "index_items_on_list_id"
